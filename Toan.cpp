5adb15ae5c5b59ea8187c67cb8323664098d0a27
duytoan2003 <135208678+duytoan2003@users.noreply.github.com>
2023-06-05 01:06:53 +0700

    hi

---
+++ Toan.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Nhanh A"<<endl;
+    std::cout<<"Nhanh Toan"<<endl;
+
+    std::cout<<"Hello"<<endl;
+
+}
---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Nhanh A"<<endl;
+    std::cout<<"Nhanh tuyen"<<endl;
+
+    std::cout<<"nhanh main"<<endl;
+    std::cout<<"nhanh main pull"<<endl;
+
+
+}
